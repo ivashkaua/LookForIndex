fbe58e2807b012ff6db11f0d8636df84c097aecf
ivashkaua <86633081+ivashkaua@users.noreply.github.com>
2021-09-22 19:43:55 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LookForIndex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество элементов массива: \t");
+            int ArrNumber = int.Parse(Console.ReadLine());
+            int[] array = new int[ArrNumber];
+
+            for (int l = 0; l < ArrNumber; l++)
+            {
+                Console.Write($"\nВведите число с индексом {l} \t");
+                array[l] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine();
+            Console.Write("Введите число для определения индекса: \t");
+            int value = int.Parse(Console.ReadLine());
+            int index = Array.IndexOf(array, value);
+            Console.WriteLine("\nИндекс числа в массиве - " + index);
+            Console.ReadLine();
+        }
+    }
+}
